e68bd10438d6006f7628b21fe30bef48ffc9611e
umatbro <umatbroo@gmail.com>
2018-08-07 21:54:20 +0200

    users and groups in api2

--- crawler/urls.py
+++ crawler/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
+from django.urls import path
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include('collector.urls')),
+    url(r'^api2/', include('api2.urls')),
 ]
