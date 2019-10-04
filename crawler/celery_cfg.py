7dfe0ddcf4dd1db29cb22755b7643a07ac60ba6f
umatbro <umatbroo@gmail.com>
2019-10-04 14:35:39 +0200

    Add beat (#12)
    * Add beat
    * Remove bind from task
    * Add celery timezone

--- crawler/celery_cfg.py
+++ crawler/celery_cfg.py
@@ -1,8 +1,24 @@
 import os
 
+from django.core.management import call_command
+
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crawler.settings')
 app = Celery('crawler')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+
+@app.task
+def update_db():
+    call_command('updatedb')
+
+
+app.conf.beat_schedule = {
+    'update_db': {
+        'task': 'crawler.celery_cfg.update_db',
+        'schedule': crontab(day_of_week='*', hour='0,12', minute='0'),
+    },
+}
